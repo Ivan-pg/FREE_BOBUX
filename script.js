b54de6dfacc3e1b1a12e8bd5c19ead896e207fbc
Ivan-pg <108332452+Ivan-pg@users.noreply.github.com>
2022-07-09 16:10:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function getInputValue() {
+        let inputVal = document.getElementById("inputId").value;
+        if (inputVal=="piglet_g27"){alert("How would you know...?");}
+        else if(inputVal==""){alert("Type something");}
+        else if(inputVal=="hello"){alert("Hello, dear 192.0.2.1.");}
+        else if(inputVal=="bobux"){alert("Yea, just click submit and it's yours!");}
+        else if(inputVal=="roblox"){alert("Some weird shitty game");}
+        else if(inputVal=="gru"){alert("REAL???????");}
+        else if(inputVal=="nefario"){alert("Fard moment");}
+        else if(inputVal=="sex"){alert("WHAT DID YOU JUST SAY???");}
+        else if(inputVal=="bruh"){alert("Amogus");}
+        else if(inputVal=="amogus"){alert("bruh");}
+        else if(inputVal=="69"){alert("420");}
+        else if(inputVal=="420"){alert("69");}
+        else if(inputVal=="rickroll"){alert("Totally not)");}
+        else if(inputVal=="account name"){alert("You clever mfer");}
+        else if(inputVal=="obama"){alert("Hamburger");}
+        else if(inputVal=="goodbye"){alert("See you");}
+        else if(inputVal=="c++"){alert("You should use C instead, it's closer to the hardware");}
+      }
